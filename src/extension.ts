c5e88e39eb5901bff312f01c2cb77639cf0dde9c
RedSuns Chan <redsunschan@gmail.com>
2022-08-01 09:39:15 +0800

    fix some ide warning

--- src/extension.ts
+++ src/extension.ts
@@ -4,10 +4,12 @@ import { mapping } from "./mapping";
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('HEX Utilities plugin installed');
-	Object.entries(mapping).forEach(([key, value], index) => {
+	Object.entries(mapping).forEach(([key, value]) => {
 		context.subscriptions.push(vscode.commands.registerCommand('hex-utils.' + key, value));
 	});
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() { 
+	console.log('HEX Utilities plugin deactivated');
+}
